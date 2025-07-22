12da4a7856666bf7ace2f3bc8cd9759a2a3f30d0
Varlen123 <varlengaspar171@gmail.com>
2025-07-22 17:59:32 -0300

    fix: Adiciona o final na chamada para evitar null

--- src/main/java/com/cinesphere/cinesphere/service/CategoryService.java
+++ src/main/java/com/cinesphere/cinesphere/service/CategoryService.java
@@ -4,26 +4,23 @@ import java.util.List;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import com.cinesphere.cinesphere.entity.CategoryEntity;
 import com.cinesphere.cinesphere.repository.CategoryRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
     
     private final CategoryRepository categoryRepository;
 
-    @Autowired
-    public CategoryService(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
-
     public List<CategoryEntity> getCategories(){
         return categoryRepository.findAll();
     }
-
 
     public CategoryEntity saveCategory(CategoryEntity category){
         return categoryRepository.save(category);
